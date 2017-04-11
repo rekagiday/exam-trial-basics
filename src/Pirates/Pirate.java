fbb872e91b158a8b72d0f3ff53c1a075ebdfe985
Reka Giday <giday.r@gmail.com>
2017-04-11 11:21:39 +0200

    create function to count specific pirates

---
+++ src/Pirates/Pirate.java
@@ -0,0 +1,8 @@
+package Pirates;
+
+/**
+ * Created by User on 2017. 04. 11..
+ */
+public class Pirate {
+
+}
--- src/Pirates/PirateCounter.java
+++ src/Pirates/PirateCounter.java
@@ -3,6 +3,41 @@ package Pirates;
 /**
  * Created by User on 2017. 04. 11..
  */
+
+import java.util.*;
+
 public class PirateCounter {
 
+  static class Pirate {
+
+    String name;
+    boolean hasWoodenLeg;
+    int gold;
+
+    Pirate(String name, boolean hasWoodenLeg, int gold) {
+      this.name = name;
+      this.hasWoodenLeg = hasWoodenLeg;
+      this.gold = gold;
+    }
+  }
+
+  private List<Pirate> countPirates(List<Pirate> listOfPirates) {
+    List<Pirate> selectedPirates = new ArrayList<>();
+    for (Pirate pirate : listOfPirates) {
+      if (pirate.hasWoodenLeg && pirate.gold > 15) {
+        selectedPirates.add(pirate);
+      }
+    }
+    return selectedPirates;
+  }
+
+  public static void main(String... args) {
+    ArrayList<Pirate> pirates = new ArrayList<>();
+
+    pirates.add(new Pirate("Olaf", false, 12));
+    pirates.add(new Pirate("Uwe", true, 9));
+    pirates.add(new Pirate("Jack", true, 16));
+    pirates.add(new Pirate("Morgan", false, 17));
+    pirates.add(new Pirate("Hook", true, 20));
+  }
 }
