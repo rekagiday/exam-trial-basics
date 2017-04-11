72348196038b4293939254a53eb900d545eebe7c
Reka Giday <giday.r@gmail.com>
2017-04-11 10:27:10 +0200

    add cuboid surface and volume counter exercise

--- src/Cuboid/Cuboid.java
+++ src/Cuboid/Cuboid.java
@@ -5,4 +5,31 @@ package Cuboid;
  */
 public class Cuboid {
 
+  int height;
+  int length;
+  int depth;
+  int volume;
+  int surface;
+
+  public Cuboid(int height, int length, int depth) {
+    this.height = height;
+    this.length = length;
+    this.depth = depth;
+  }
+
+  void calculateVolume() {
+    volume = height * length * depth;
+    System.out.println("The volume of your cuboid is: " + volume);
+  }
+
+  void calculateSurface() {
+    surface = ((height * length) + (height * depth) + (length * depth)) * 2;
+    System.out.println("The surface of your cuboid is: " + surface);
+  }
+
+  public static void main(String[] args) {
+  Cuboid cuboid = new Cuboid(3,4,5);
+  cuboid.calculateSurface();
+  cuboid.calculateVolume();
+  }
 }
